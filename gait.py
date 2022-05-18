84b5f40afcd6146291167a0e1ff48fda4bf4343a
velythyl <charlie.gauthier@umontreal.ca>
2022-05-18 17:47:08 -0400

    works for 1 actuator

--- gait.py
+++ gait.py
@@ -7,16 +7,17 @@ from torch import nn as nn
 class Gait(nn.Module):
     def __init__(self, nb_gaussians, action_shape):
         super().__init__()
-        self.mixture_dim = (nb_gaussians, action_shape[0])
+        self.mixture_dim = (action_shape[0], nb_gaussians)
+
+        # initial period is to have a ~25 frame period. Learnable parameter.
+        self.period_b = nn.Parameter(torch.tensor(0.25), requires_grad=True)
 
         # same init as
         # https://github.com/JeremyLinux/PyTorch-Radial-Basis-Function-Layer/blob/master/Torch%20RBF/torch_rbf.py
-        self.mu_matrix = nn.Parameter(torch.normal(0,1,self.mixture_dim), requires_grad=True)
+        self.mu_matrix = nn.Parameter(torch.linspace(0, 2*np.pi, nb_gaussians).expand(*self.mixture_dim), requires_grad=True)
         self.sigma_matrix = nn.Parameter(-torch.ones(self.mixture_dim), requires_grad=True)
 
         self.weights = nn.Parameter(torch.normal(0,1,self.mixture_dim), requires_grad=True)
-        # initial period is to have a ~25 frame period. Learnable parameter.
-        self.period_b = nn.Parameter(torch.tensor(0.25), requires_grad=True)
 
     @property
     def period(self):
@@ -37,52 +38,62 @@ class Gait(nn.Module):
         return gaussian
 
     def forward(self, frame_nb):
-        frame_nb = self.frame2percent(frame_nb).unsqueeze(-1)
-        frame_nb_batch = frame_nb.unsqueeze(-1).expand(frame_nb.shape[0], *self.mixture_dim)
+        frame_nb = self.frame2percent(frame_nb) * np.pi * 2
 
-        DEBUG = frame_nb_batch.detach().numpy()
+        frame_nb_batch = frame_nb.repeat(frame_nb.shape[0], *self.mixture_dim)
 
-        mixed = self.cyclic_gaussian_mixture(frame_nb_batch)
-        DEBUG = mixed.detach().numpy()
+        frame_nb_batch = frame_nb.unsqueeze(-1).unsqueeze(-1).expand(frame_nb.shape[0], *self.mixture_dim)
 
-        #mixed_weighted = torch.mul(mixed, self.weights)
+        DEBUG = frame_nb_batch.squeeze().detach().numpy()
+
+        mixed = self.cyclic_gaussian_mixture(frame_nb_batch).squeeze()
+        DEBUG = mixed.squeeze().detach().numpy()
+
+        mixed_weighted = torch.mul(mixed, self.weights)
 
         #print(mixed.shape)
 
-        #mixed = torch.sum(mixed, dim=1)
-        #mixed_weighted = torch.sum(mixed_weighted, dim=1)
+        mixed = torch.sum(mixed, dim=-1)
+        mixed_weighted = torch.sum(mixed_weighted, dim=-1)
 
-       # activations = mixed_weighted / mixed
+        activations = mixed_weighted / mixed
 
         #print(activations.shape)
 
-        return torch.sum(mixed, dim=1)
+        #final = torch.sum(mixed, dim=-1)
+
+        #DEBUG = final.squeeze().detach().numpy()
+
+        #return final
+        return activations
 
 if __name__ == "__main__":
     FRAMES = 50
-    gait = Gait(100, [1])
+    gait = Gait(500, [2])
     opt = torch.optim.Adam(gait.parameters())
 
     x = torch.arange(0, FRAMES, requires_grad=False)
 
-    y = torch.normal(0.2, 1, (FRAMES,), requires_grad=False)
-    z = torch.normal(0.2, 1, (FRAMES,), requires_grad=False)
-    y = y + z
+    y = torch.normal(0.2, 1, (FRAMES,2), requires_grad=False)
+    y2 = torch.normal(0.2, 1, (FRAMES,2), requires_grad=False)
+    y = y + y2
+
+    target = y
 
     plt.plot(x, y)
     plt.plot(gait(x).detach().cpu().numpy())
     plt.show()
 
     mse = torch.nn.MSELoss()
-    for i in range(100000):
-        y_pred = gait(x)
-        loss = mse(y_pred, y)
+    for i in range(10000):
+        y_z_pred = gait(x)
+        loss = mse(y_z_pred, target)
         opt.zero_grad()
         loss.backward()
         opt.step()
         print(loss.detach().item())
 
     plt.plot(x, y)
-    plt.plot(y_pred.detach().cpu().numpy())
+    plt.plot(y_z_pred.detach().cpu().numpy())
     plt.show()
     pass
