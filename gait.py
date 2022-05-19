f9649834591f4d3366d5b8bc63d23615614c9319
velythyl <charlie.gauthier@umontreal.ca>
2022-05-19 14:18:07 -0400

    gait works on toy dataset finally

--- gait.py
+++ gait.py
@@ -3,97 +3,76 @@ import torch
 from matplotlib import pyplot as plt
 from torch import nn as nn
 
+TAU = np.pi * 2
 
 class Gait(nn.Module):
-    def __init__(self, nb_gaussians, action_shape):
+    def __init__(self, nb_gaussians, action_shape, n_frame_repeat):
         super().__init__()
         self.mixture_dim = (action_shape[0], nb_gaussians)
 
         # initial period is to have a ~25 frame period. Learnable parameter.
-        self.period_b = nn.Parameter(torch.tensor(1.), requires_grad=True)
+        self.period_b = nn.Parameter(torch.tensor(TAU / n_frame_repeat), requires_grad=True)
 
-        # same init as
-        mu_matrix_init = np.linspace(0, 2*np.pi, nb_gaussians + 1)
+        # same init as https://github.com/studywolf/pydmps/blob/master/pydmps/dmp_rhythmic.py
+        mu_matrix_init = np.linspace(0, TAU, nb_gaussians + 1)
         mu_matrix_init = mu_matrix_init[0:-1]
-        mu_matrix_init = torch.tensor(action_shape[0]*[mu_matrix_init])
-        # https://github.com/JeremyLinux/PyTorch-Radial-Basis-Function-Layer/blob/master/Torch%20RBF/torch_rbf.py
-        self.mu_matrix = mu_matrix_init.float() #nn.Parameter(mu_matrix_init.float(), requires_grad=False)
-        self.sigma_matrix = nn.Parameter(-torch.ones(self.mixture_dim), requires_grad=True)
+        mu_matrix_init = torch.tensor(action_shape[0] * [mu_matrix_init])
 
-        self.weights = nn.Parameter(torch.normal(0,1,self.mixture_dim), requires_grad=True)
+        self.mu_matrix = nn.Parameter(mu_matrix_init.float(), requires_grad=True)
+        self.sigma_matrix = nn.Parameter(-torch.ones(self.mixture_dim), requires_grad=True)
+        self.weights = nn.Parameter(torch.normal(0, 1, self.mixture_dim), requires_grad=True)
 
     def period(self):
-        return (np.pi * 2) / self.period_b # .clone().detach().item()
+        return TAU / self.period_b.detach().item()
 
-    def frame2percent(self, frame_nb: torch.Tensor):    # only to be used as reference for the actor to know which part of the cycle we're in
-        return frame_nb / self.period
+    def frame2percent(self, frame_nb):
+        percent = frame_nb * self.period_b
+        percent = torch.remainder(percent, TAU)
+        percent = percent / TAU
+        return percent
 
     def cyclic_gaussian_mixture(self, frame_numbers):
         # https://studywolf.wordpress.com/tag/rhythmic-dynamic-movement-primitives/
-
         x_mu = frame_numbers - self.mu_matrix
-
-        DEBUG = x_mu.squeeze().detach().numpy()
-
         cos_x_mu = torch.cos(self.period_b * x_mu)
-
         scaled_x_mu_pow = torch.mul(self.sigma_matrix, cos_x_mu) - 1
-
         gaussian = torch.exp(scaled_x_mu_pow)
         return gaussian
 
     def forward(self, frame_nb):
-        #frame_nb = frame_nb / self.period_b
-
-       # frame_nb = self.frame2percent(frame_nb) * np.pi * 2
-
-        #frame_nb_batch = frame_nb.repeat(frame_nb, *self.mixture_dim)
-
         frame_nb_batch = frame_nb.unsqueeze(-1).unsqueeze(-1).expand(frame_nb.shape[0], *self.mixture_dim)
 
-        DEBUG = frame_nb_batch.squeeze().detach().numpy()
-
         mixed = self.cyclic_gaussian_mixture(frame_nb_batch)
-        DEBUG = mixed.squeeze().detach().numpy()
-
         mixed_weighted = torch.mul(mixed, self.weights)
-
-        #print(mixed.shape)
 
         mixed = torch.sum(mixed, dim=-1)
         mixed_weighted = torch.sum(mixed_weighted, dim=-1)
 
-        activations = mixed_weighted #mixed_weighted / mixed
+        activations = mixed_weighted / mixed
 
-        DEBUG2 = activations.squeeze().detach().numpy()
-
-        #print(activations.shape)
-
-        #final = torch.sum(mixed, dim=-1)
-
-        #DEBUG = final.squeeze().detach().numpy()
-
-        #return final
         return activations
+
 
 if __name__ == "__main__":
     FRAMES = 50
-    gait = Gait(500, [1])
+    gait = Gait(50, [2], n_frame_repeat=int(FRAMES/2))
     opt = torch.optim.Adam(gait.parameters())
 
     x = torch.arange(0, FRAMES, requires_grad=False)
 
-    y = torch.normal(0.2, 1, (FRAMES,1), requires_grad=False)
-    y2 = torch.normal(0.2, 1, (FRAMES,1), requires_grad=False)
+    y = torch.normal(0.2, 1, (int(FRAMES / 2), 2), requires_grad=False)
+    y2 = torch.normal(0.2, 1, (int(FRAMES / 2), 2), requires_grad=False)
     y = y + y2
+    y = torch.cat((y, y))
 
     target = y
 
     plt.plot(x, y)
+    # plt.plot(x, torch.cos(x))
     plt.plot(gait(x).detach().cpu().numpy())
     plt.show()
 
-    print(gait.period)
+    before = gait.period_b.detach().item()
 
     mse = torch.nn.MSELoss()
     for i in range(10000):
@@ -107,9 +86,13 @@ if __name__ == "__main__":
         opt.step()
         print(loss.detach().item())
 
-    print(gait.period_b)
+    print('before', before)
+    print('after', gait.period_b.detach().item())
 
     plt.plot(x, y)
-    plt.plot(y_z_pred.detach().cpu().numpy())
+    # plt.plot(x, torch.cos(x))
+    x = torch.linspace(0, 50, 5000)
+    y_z_pred = gait(x)
+    plt.plot(x.detach().cpu().numpy(), y_z_pred.detach().cpu().numpy())
     plt.show()
     pass
